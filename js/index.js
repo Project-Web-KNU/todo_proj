440249de05c3626a97c50b4f504b5bbb169a164d
BBlack_Hun <h1103j@kangwon.ac.kr>
2022-01-06 00:44:43 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,27 @@
-let Todolist = function(){
+let Todolist = function () {
     // Date로 today와 date변수를 선언
     let today = new Date();
     let date = new Date();
     let month = "";
+    let posts = "";
 
-    $("a[name=preMon]").click(function() { // 이전달
+    $("a[name=preMon]").click(function () { // 이전달
         $("#calendar > tbody > td").remove();
         $("#calendar > tbody > tr").remove();
-        today = new Date ( today.getFullYear(), today.getMonth()-1, today.getDate());
+        today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
         buildCalendar();
     })
-    
-    $("a[name=nextMon]").click(function(){ //다음달
+
+    $("a[name=nextMon]").click(function () { //다음달
         $("#calendar > tbody > td").remove();
         $("#calendar > tbody > tr").remove();
-        today = new Date ( today.getFullYear(), today.getMonth()+1, today.getDate());
+        today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
         buildCalendar();
     })
 
     // 달력을 만들어 주는 함수
-    function buildCalendar(option) {
-        if(option) {
+    async function buildCalendar(option) {
+        if (option) {
             today = new Date();
             date = new Date();
             month = "";
@@ -29,147 +30,189 @@ let Todolist = function(){
             let nowYear = today.getFullYear(); //2021
             let nowMonth = today.getMonth(); // 11
             let firstDay = new Date(nowYear, nowMonth, 1).getDay();
-            let lastDate = new Date(nowYear, nowMonth+1, 0).getDate();
+            let lastDate = new Date(nowYear, nowMonth + 1, 0).getDate();
 
-            if ((nowYear%4===0 && nowYear % 100 !== 0) || nowYear%400 === 0) {
+            if ((nowYear % 4 === 0 && nowYear % 100 !== 0) || nowYear % 400 === 0) {
                 lastDate[1] = 29;
             }
             $('#year').text(nowYear);
-            month = transMonthName(nowMonth+1);
+            month = transMonthName(nowMonth + 1);
             $(".year_mon").text(month);
+
+            // 전체 일정을 호출!
+            const response = await fetch('/api/v1/todolist/list', {
+                method: 'GET'
+            });
             
-            
-            for (i=0; i <firstDay; i++) { // 첫번째 줄 빈칸
+            // 오류가 없을시.. 해당 내용 콘솔에 출력
+            if (response.status === 200) {
+                posts = await response.json();
+            }
+
+            for (i = 0; i < firstDay; i++) { // 첫번째 줄 빈칸
                 $("#calendar tbody:last").append("<td></td>");
             }
 
-            for (i=1; i<=lastDate; i++) {
+            for (i = 1; i <= lastDate; i++) {
+                html = [];
                 plusDate = new Date(nowYear, nowMonth, i).getDay(); // 12월 1일은? 수요일 -> 3
-                let daytoday = nowYear + "-" + nowMonth + "-" + plusDate;
+                let daytoday = nowYear + "-" + (nowMonth + 1) + "-" + i;
+                // date에 필터를 걸어서 해당 하는 날에 있는 메모들만 추출
+                let testPost = posts.memos.filter((item)=> item.date === daytoday);
+                for (post in testPost) {
+                    if (html.length > 2) {
+                        html.push(`<p>(총${testPost.length}개)</p>`);
+                        break;
+                    }
+                    postTag  = `<p style="text-overflow: ellipsis;white-space : nowrap;overflow : hidden;width:85px">${testPost[post].memo}</p>`;
+                    html.push(postTag);
+                };
+                html = html.join("");
                 if (plusDate == 0) {
                     $('#calendar tbody:last').append("<tr></tr>");
                 }
-                $('#calendar tbody:last').append(`<td class='date' style="cursor: pointer;"onclick='Todolist.showPopup(this);' value=${daytoday}>`+ i + "</td>");
+                $('#calendar tbody:last').append(`<td class='date' style="cursor: pointer;"onclick='Todolist.showPopup($(this).attr("value"));' value=${daytoday}>` + i +`<div>${html}</div>` + "</td>");
             }
             console.log($("#calendar > tbody > td").length); // 34^^
-            if($("#calendar > tbody > td").length%7 != 0) {
+            if ($("#calendar > tbody > td").length % 7 != 0) {
                 // 나머지가 6이기 때문에 통과
-                for(i=1; i<= $('#calendar > tbody > td').length%7; i++) {
+                for (i = 1; i <= $('#calendar > tbody > td').length % 7; i++) {
                     $('#calendar tbody:last').append("<td></td>");
                 }
             }
 
-            $(".date").each(function(index){ // 오늘 날짜 표시
+            $(".date").each(function (index) { // 오늘 날짜 표시
                 let idx = $(".date").eq(index).text()
                 // console.log(idx);
-                if(nowYear == date.getFullYear() && nowMonth == date.getMonth() && $(".date").eq(index).text() == date.getDate()) {
+                if (nowYear == date.getFullYear() && nowMonth == date.getMonth() && $(".date").eq(index).text() == date.getDate()) {
                     $(".date").eq(index).addClass('colToday');
-                    $(".colToday").css('background-color','pink');
+                    $(".colToday").css('background-color', 'pink');
                 }
-            }) 
+            })
         }
         else {
             let nowYear = today.getFullYear(); //2021
             let nowMonth = today.getMonth(); // 11
             let firstDay = new Date(nowYear, nowMonth, 1).getDay();
-            let lastDate = new Date(nowYear, nowMonth+1, 0).getDate();
+            let lastDate = new Date(nowYear, nowMonth + 1, 0).getDate();
 
-            if ((nowYear%4===0 && nowYear % 100 !== 0) || nowYear%400 === 0) {
+            if ((nowYear % 4 === 0 && nowYear % 100 !== 0) || nowYear % 400 === 0) {
                 lastDate[1] = 29;
             }
             $('#year').text(nowYear);
-            month = transMonthName(nowMonth+1);
+            month = transMonthName(nowMonth + 1);
             $(".year_mon").text(month);
+
+            // 전체 일정을 호출!
+            const response = await fetch('/api/v1/todolist/list', {
+                method: 'GET'
+            });
             
-            
-            for (i=0; i <firstDay; i++) { // 첫번째 줄 빈칸
+            // 오류가 없을시.. 해당 내용 콘솔에 출력
+            if (response.status === 200) {
+                posts = await response.json();
+            }
+
+            for (i = 0; i < firstDay; i++) { // 첫번째 줄 빈칸
                 $("#calendar tbody:last").append("<td></td>");
             }
 
-            for (i=1; i<=lastDate; i++) {
+            for (i = 1; i <= lastDate; i++) {
+                html = [];
                 plusDate = new Date(nowYear, nowMonth, i).getDay(); // 12월 1일은? 수요일 -> 3
-                let daytoday = nowYear + "-" + (nowMonth+1) + "-" + i;
+                let daytoday = nowYear + "-" + (nowMonth + 1) + "-" + i;
+                // date에 필터를 걸어서 해당 하는 날에 있는 메모들만 추출
+                let testPost = posts.memos.filter((item)=> item.date === daytoday);
+                for (post in testPost) {
+                    if (html.length > 2) {
+                        html.push(`<p>(총${testPost.length}개)</p>`);
+                        break;
+                    }
+                    postTag  = `<p style="text-overflow: ellipsis;white-space : nowrap;overflow : hidden;width:85px">${testPost[post].memo}</p>`;
+                    html.push(postTag);
+                };
+                html = html.join("");
                 if (plusDate == 0) {
                     $('#calendar tbody:last').append("<tr></tr>");
                 }
-                $('#calendar tbody:last').append(`<td class='date' style="cursor: pointer;"onclick='Todolist.showPopup($(this).attr("value"));' value=${daytoday}>`+ i + "</td>");
+                $('#calendar tbody:last').append(`<td class='date' style="cursor: pointer;"onclick='Todolist.showPopup($(this).attr("value"));' value=${daytoday}>` +`<span class ="days">${i}</span>` +`<div>${html}</div>` + "</td>");
             }
             console.log($("#calendar > tbody > td").length); // 34^^
-            if($("#calendar > tbody > td").length%7 != 0) {
+            if ($("#calendar > tbody > td").length % 7 != 0) {
                 // 나머지가 6이기 때문에 통과
-                for(i=1; i<= $('#calendar > tbody > td').length%7; i++) {
+                for (i = 1; i <= $('#calendar > tbody > td').length % 7; i++) {
                     $('#calendar tbody:last').append("<td></td>");
                 }
             }
 
-            $(".date").each(function(index){ // 오늘 날짜 표시
-                let idx = $(".date").eq(index).text()
-                if(nowYear == date.getFullYear() && nowMonth == date.getMonth() && $(".date").eq(index).text() == date.getDate()) {
+            $(".days").each(function (index) { // 오늘 날짜 표시
+                let days = $('.days').eq(index).text();
+                if (nowYear == date.getFullYear() && nowMonth == date.getMonth() && days == date.getDate()) {
                     $(".date").eq(index).addClass('colToday');
-                    $(".colToday").css('background-color','pink');
+                    $(".colToday").css('background-color', 'pink');
                 }
-            }) 
+            })
         }
     }
 
     function transMonthName(monthNum) {
-        let month ="";
-        switch(monthNum) {
+        let month = "";
+        switch (monthNum) {
             case 1:
-                month="January";
+                month = "January";
                 break;
             case 2:
-                month="February";
+                month = "February";
                 break;
             case 3:
-                month="March";
+                month = "March";
                 break;
             case 4:
-                month="April";
+                month = "April";
                 break;
             case 5:
-                month="May";
+                month = "May";
                 break;
             case 6:
-                month="June";
+                month = "June";
                 break;
             case 7:
-                month="July";
+                month = "July";
                 break;
             case 8:
-                month="August";
+                month = "August";
                 break;
             case 9:
-                month="September";
+                month = "September";
                 break;
             case 10:
-                month="October";
+                month = "October";
                 break;
             case 11:
-                month="November";
+                month = "November";
                 break;
             case 12:
-                month="December";
+                month = "December";
                 break;
             default:
                 break;
         }
-    
+
         return month
     }
-    
+
     function showPopup(obj) {
         let popup_value = obj
         console.log(popup_value);
-        window.open("popup.html?data="+popup_value, "_blank", "width=500, height=480, left=200, top=100, resizable=no"); 
+        window.open("popup.html?data=" + popup_value, "popup", "width=500, height=480, left=200, top=100, resizable=no");
     }
 
-    return { 
+    return {
         init: function () {
             buildCalendar(false);
         },
-        buildCalendar : buildCalendar,
-        showPopup : showPopup
+        buildCalendar: buildCalendar,
+        showPopup: showPopup
     }
 }();
 
--- js/sub.js
+++ js/sub.js
@@ -1,37 +1,71 @@
 let TodoListPopUp = function () {
 
-    let param = ""
+    let param = "";
 
     async function setParam(data) {
         let obj = {};
         let url = decodeURI(data);
+        // url = ?data=2022-1-5
         url = url.slice(1, url.length);
+        // url = data=2022-1-5
         param = url.split('=')[1];
+        // ['data', '2022-1-5']
+        // param = 2022-1-5
     }
 
     async function post_findAllTodo() {
-        const response = await fetch('/api/v1/todolist/list?date=' + param, {
+        const response = await fetch('/api/v1/todolist/listone?date=' + param, {
             method: 'GET',
-        })
-
+        });
         if (response.status === 200) {
-            let list = document.getElementById('list')
+            let list = document.getElementById('list');
             list.innerHTML = '';
             let html = '';
             const post = await response.json();
-            console.log(post);
-            console.log(post.count);
             for (item in post.memos) {
                 let memo = post.memos[item].memo;
-                let postTag = `<tr><th scope="row"><i class="far fa-check-circle"></i></th><td>${memo}</td></tr>`;
+                let postTag = `<tr><th scope="row" onclick="TodoListPopUp.check(1);"><i class="far fa-check-circle"></i></th><td style="width:70%;">${memo}</td>
+                <td style='text-align: center;'><i style="margin-right: 10px;" class="far fa-edit"></i><i class="far fa-trash-alt"></i></td></tr>`;
                 html += postTag;
             }
             list.insertAdjacentHTML('afterbegin', html);
+        };
+    }
+
+    async function submit() {
+        let memo = document.getElementById('text').value;
+        const response = await fetch('/api/v1/todolist/create', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                date: param,
+                memo: memo
+            })
+        });
+
+        if (response.status === 201) {
+            alert("일정을 추가하였습니다.");
+            location.reload();
+        } else if (response.status === 400) {
+            alert('3자 이상 50자 이하로 입력하여주세요!')
+        } else {
+            alert("할 일을 입력해주세요!");
         }
     }
 
-    function submit() {
-        alert("일정을 추가하였습니다." + param);
+    async function check(id) {
+        alert("완료!")
+
+    }
+
+    async function edit(id){
+
+    }
+
+    async function del(id) {
+
     }
 
     return {
@@ -40,17 +74,16 @@ let TodoListPopUp = function () {
             post_findAllTodo();
         },
         setParam: setParam,
-        submit: submit
+        submit: submit,
+        check: check,
+        edit: edit,
+        del: del
 
     }
 }();
 
 document.addEventListener('DOMContentLoaded', function () {
     let data = window.location.search;
+    // ?data=2022-1-5
     TodoListPopUp.init(data);
 });
-
-function submit() {
-    let data = document.querySelector('text').innerHTML;
-    document.write(td);
-}
